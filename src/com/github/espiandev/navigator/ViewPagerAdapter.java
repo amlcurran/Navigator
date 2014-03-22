5193a21c557b0c516cb7e591e382ed8eca7779e8
Alex Curran <aml.curran@gmail.com>
2014-03-22 11:58:48 +0000

    Initial commit of shell project

---
+++ src/com/github/espiandev/navigator/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.github.espiandev.navigator;
+
+public class ViewPagerAdapter {
+}
---
+++ test/com/github/espiandev/navigator/ViewPagerAdapterTest.java
@@ -0,0 +1,14 @@
+package com.github.espiandev.navigator;
+
+import org.junit.Before;
+
+public class ViewPagerAdapterTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+
+}
